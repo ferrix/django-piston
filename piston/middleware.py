12d2b12b7501ebdb15d108d0de40a5f9b2d61aee
Ferrix Hovi <ferrix@ferrix.fi>
2012-01-15 17:34:18 +0200

    Add ContentTypeMiddleware to remove errors

--- piston/middleware.py
+++ piston/middleware.py
@@ -18,3 +18,8 @@ def compat_middleware_factory(klass):
 
 ConditionalMiddlewareCompatProxy = compat_middleware_factory(ConditionalGetMiddleware)
 CommonMiddlewareCompatProxy = compat_middleware_factory(CommonMiddleware)
+
+class ContentTypeMiddleware(object):
+    def process_request(self, request):
+        if request.method in ('POST', 'PUT') and request.META['CONTENT_TYPE'] > 0:
+            request.META['CONTENT_TYPE'] = [c.strip() for c in request.META['CONTENT_TYPE'].split(';')][0]
